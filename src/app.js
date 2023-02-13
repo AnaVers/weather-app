53d5da26309ee91e45476233a8de7fa9bbac936f
AnaVers <123906728+AnaVers@users.noreply.github.com>
2023-02-13 21:44:49 +0100

    Adding the search function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+let apiKey = "16dbd1b981cb512ad69b8f3c1794a804";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid={API key}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
